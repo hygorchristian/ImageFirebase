e4c046c183e6d85b5e6a4f1e5495b03612a5e853
hygor <hygorinuyasha@hotmail.com>
2018-05-19 22:53:46 -0300

    Initial commit

---
+++ app/src/main/java/br/com/celebrimbor/imagefirebase/Image.java
@@ -0,0 +1,13 @@
+package br.com.celebrimbor.imagefirebase;
+
+import android.graphics.drawable.Drawable;
+
+public class Image {
+
+    public String image;
+    public Drawable imageDrw;
+    public String name;
+    public String brief;
+    public Integer counter = null;
+
+}
